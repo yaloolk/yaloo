5f49072df344ed5a45860fe8850050855ea0efe1
naflanmohamed <naflanmohammed611@gmail.com>
2025-11-04 15:35:02 +0500

    app_text_styles.dart File Created

---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+  static final TextStyle headlineLarge = GoogleFonts.poppins(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryBlue,
+  );
+
+  static final TextStyle headlineMedium = GoogleFonts.poppins(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle bodyLarge = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryGray,
+  );
+
+  static final TextStyle bodySmall = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    color: AppColors.primaryGray,
+  );
+
+  static final TextStyle buttonText = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+}
